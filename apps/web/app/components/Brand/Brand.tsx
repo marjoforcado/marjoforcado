6f18000b623de7033b3ba824c711d7dfc58a6405
marjoforcado <marjoforcado@gmail.com>
2024-01-13 23:49:38 +0800

    Add spacing

--- apps/web/app/components/Brand/Brand.tsx
+++ apps/web/app/components/Brand/Brand.tsx
@@ -1,7 +1,7 @@
 import styles from "./styles.module.scss";
 
 const Brand = () => {
-  return <p className={styles.brand}>Moon Landing Projects</p>;
+  return <p className={styles.brand}>mrjfrcdo</p>;
 };
 
 export default Brand;
