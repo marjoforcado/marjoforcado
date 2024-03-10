54d86776c6e5e6cad8d9a7b05515f8ca5db7b397
marjoforcado <marjoforcado@gmail.com>
2024-03-10 11:19:41 +0800

    Add Button component and update Text component

--- apps/web/app/components/Text/Text.tsx
+++ apps/web/app/components/Text/Text.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/lib/utils";
+
 type TProps = {
   variant?:
     | "h1"
@@ -10,15 +12,21 @@ type TProps = {
     | "small"
     | "muted";
   children: string;
+  className?: string;
 };
 
 export const Text = (props: TProps) => {
-  const { variant = "p", children } = props;
+  const { variant = "p", children, className } = props;
 
   switch (variant) {
     case "h1": {
       return (
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        <h1
+          className={cn(
+            "scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl",
+            className
+          )}
+        >
           {children}
         </h1>
       );
@@ -26,7 +34,12 @@ export const Text = (props: TProps) => {
 
     case "h2": {
       return (
-        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+        <h2
+          className={cn(
+            "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+            className
+          )}
+        >
           {children}
         </h2>
       );
@@ -34,7 +47,12 @@ export const Text = (props: TProps) => {
 
     case "h3": {
       return (
-        <h3 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+        <h3
+          className={cn(
+            "scroll-m-20 text-2xl font-semibold tracking-tight",
+            className
+          )}
+        >
           {children}
         </h3>
       );
@@ -42,32 +60,53 @@ export const Text = (props: TProps) => {
 
     case "h4": {
       return (
-        <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
+        <h4
+          className={cn(
+            "scroll-m-20 text-xl font-semibold tracking-tight",
+            className
+          )}
+        >
           {children}
         </h4>
       );
     }
 
     case "p": {
-      return <p className="leading-7 [&:not(:first-child)]:mt-6">{children}</p>;
+      return (
+        <p className={cn("leading-7 [&:not(:first-child)]:mt-6", className)}>
+          {children}
+        </p>
+      );
     }
 
     case "lead": {
-      return <p className="text-xl text-muted-foreground">{children}</p>;
+      return (
+        <p className={cn("text-xl text-muted-foreground", className)}>
+          {children}
+        </p>
+      );
     }
 
     case "large": {
-      return <div className="text-lg font-semibold">{children}</div>;
+      return (
+        <div className={cn("text-lg font-semibold", className)}>{children}</div>
+      );
     }
 
     case "small": {
       return (
-        <small className="text-sm font-medium leading-none">{children}</small>
+        <small className={cn("text-sm font-medium leading-none", className)}>
+          {children}
+        </small>
       );
     }
 
     case "muted": {
-      return <p className="text-sm text-muted-foreground">{children}</p>;
+      return (
+        <p className={cn("text-sm text-muted-foreground", className)}>
+          {children}
+        </p>
+      );
     }
   }
 };
