c083ef7cf80e81a0fb273af6fdfce7086cd18ead
marjoforcado <marjoforcado@gmail.com>
2024-03-10 11:45:12 +0800

    Refactor page.tsx to import NewsLetterForm component

---
+++ apps/web/client_only/forms/NewsLetterForm/NewsLetterForm.tsx
@@ -0,0 +1,29 @@
+import { Button } from "@/shadcn/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/shadcn/components/ui/card";
+import { Input } from "@/shadcn/components/ui/input";
+
+export const NewsLetterForm = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Stay up to date</CardTitle>
+        <CardDescription>
+          Get notified when I publish something new, and unsubscribe at any
+          time.
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form className="flex w-full items-center space-x-2">
+          <Input type="email" placeholder="Email" />
+          <Button type="submit">Subscribe</Button>
+        </form>
+      </CardContent>
+    </Card>
+  );
+};
