a87c919658f9460e76b19904087b0ed054c774c8
marjoforcado <marjoforcado@gmail.com>
2024-03-10 11:19:46 +0800

    Refactor ArticleCard component and add grid layout to page

--- apps/web/app/components/ArticleCard/ArticleCard.tsx
+++ apps/web/app/components/ArticleCard/ArticleCard.tsx
@@ -7,23 +7,36 @@ import {
   CardTitle,
 } from "@/shadcn/components/ui/card";
 import { Text } from "..";
+import Link from "next/link";
+import { format } from "date-fns";
 
-export const ArticleCard = () => {
+type TProps = {
+  title: string;
+  createdAt: Date;
+  children: string;
+  description: string;
+  href: string;
+};
+
+export const ArticleCard = (props: TProps) => {
+  const { title, createdAt, description, children } = props;
+
   return (
-    <Card className="border-0 shadow-none rounded-none p-0 space-y-2">
-      <CardHeader className="p-0">
-        <Text variant="muted">March 09, 2024</Text>
-        <CardTitle>Card Title</CardTitle>
-        <CardDescription>Card Description</CardDescription>
-      </CardHeader>
-      <CardContent className="p-0">
-        <Text>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Odit, harum?
-        </Text>
-      </CardContent>
-      <CardFooter className="p-0">
-        <Text>Footer</Text>
-      </CardFooter>
-    </Card>
+    <Link href="/blogs" className="block">
+      <Card className="border-0 shadow-none p-4 space-y-2 hover:bg-slate-50">
+        <CardHeader className="p-0 space-y-2">
+          <Text variant="muted">{format(createdAt, "PP")}</Text>
+          <CardTitle className="text-lg">{title}</CardTitle>
+        </CardHeader>
+        <CardContent className="p-0">
+          <Text>{children}</Text>
+        </CardContent>
+        <CardFooter className="p-0">
+          <Text variant="small" className="p-0 font-bold text-teal-600">
+            Read more
+          </Text>
+        </CardFooter>
+      </Card>
+    </Link>
   );
 };
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -14,11 +14,55 @@ export default function Page() {
     <div className="container flex flex-col gap-5 mx-auto">
       <IntroductionCard />
       <Separator />
-      <div className="space-y-10">
-        <ArticleCard />
-        <ArticleCard />
-        <ArticleCard />
-        <ArticleCard />
+      <div className="grid grid-cols-2 gap-10">
+        <div className="space-y-5">
+          <ArticleCard
+            title="Title"
+            createdAt={new Date()}
+            description="Description"
+            href="/"
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+            exercitationem.
+          </ArticleCard>
+          <ArticleCard
+            title="Title"
+            createdAt={new Date()}
+            description="Description"
+            href="/"
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+            exercitationem.
+          </ArticleCard>
+          <ArticleCard
+            title="Title"
+            createdAt={new Date()}
+            description="Description"
+            href="/"
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+            exercitationem.
+          </ArticleCard>
+          <ArticleCard
+            title="Title"
+            createdAt={new Date()}
+            description="Description"
+            href="/"
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+            exercitationem.
+          </ArticleCard>
+          <ArticleCard
+            title="Title"
+            createdAt={new Date()}
+            description="Description"
+            href="/"
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis,
+            exercitationem.
+          </ArticleCard>
+        </div>
+        <div>here</div>
       </div>
     </div>
   );
