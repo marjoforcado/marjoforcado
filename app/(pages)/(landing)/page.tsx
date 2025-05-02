0d0440d3214bc7563908e31a7afed23ccd00d7b1
Marjo Forcado <marjoforcado@gmail.com>
2025-05-02 18:36:46 +0800

    Add padding

--- app/(pages)/(landing)/page.tsx
+++ app/(pages)/(landing)/page.tsx
@@ -1,9 +1,11 @@
 export default function Page() {
   return (
-    <div className="container mx-auto border rounded-lg">
-      <span className="whitespace-pre-wrap text-center text-8xl font-semibold leading-none">
-        Mrjfrcdo.dev
-      </span>
+    <div className="px-6">
+      <div className="container mx-auto border rounded-lg">
+        <span className="whitespace-pre-wrap text-center text-8xl font-semibold leading-none">
+          Mrjfrcdo.dev
+        </span>
+      </div>
     </div>
   );
 }
