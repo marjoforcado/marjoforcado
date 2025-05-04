c87b4132321fb076febd24b64de66c99a3a2049e
Marjo Forcado <marjoforcado@gmail.com>
2025-05-04 23:49:20 +0800

    Add cursor name generator

--- app/(pages)/(landing)/page.tsx
+++ app/(pages)/(landing)/page.tsx
@@ -1,22 +1,46 @@
 "use client";
 
 import { Header } from "@/client_only/Header";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/shadcn/components/ui/context-menu";
 import { SmoothCursor } from "@/shadcn/components/ui/smooth-cursor";
-import { Fragment } from "react";
+import { generateRandomString } from "@/utils/generateRandomString";
+import { Fragment, useEffect, useState } from "react";
 
 export default function Page() {
+  const [randomString, setRandomString] = useState("");
+
+  useEffect(() => {
+    setRandomString(generateRandomString());
+  }, []);
+
   return (
     <Fragment>
-      <div className="h-[calc(100vh_-_4rem)] p-6 py-10">
-        <div className="max-w-5xl mx-auto">
-          <Header />
+      <ContextMenu>
+        <div className="h-[calc(100vh_-_4rem)] w-full relative">
+          <ContextMenuTrigger className="absolute h-full w-full p-6 py-10">
+            <div className="max-w-5xl mx-auto">
+              <Header />
+            </div>
+          </ContextMenuTrigger>
+          <ContextMenuContent className="w-64">
+            <ContextMenuItem
+              onClick={() => setRandomString(generateRandomString())}
+            >
+              Generate cursor name
+            </ContextMenuItem>
+          </ContextMenuContent>
         </div>
-      </div>
+      </ContextMenu>
       <SmoothCursor
         cursor={
           <div className="size-5 rounded-full bg-primary relative">
             <span className="absolute top-5 text-center left-1/2 transform -translate-x-1/2">
-              You
+              {randomString}
             </span>
           </div>
         }
---
+++ utils/generateRandomString.ts
@@ -0,0 +1,11 @@
+export function generateRandomString(length = 7) {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+[]{}|;:,.<>?";
+  let result = "";
+
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  return result;
+}
